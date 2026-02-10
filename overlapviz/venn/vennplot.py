3939226550752d00e3e7c2bd9d6011ab7475e4e0
Dot4diw <73592345+Dot4diw@users.noreply.github.com>
2026-02-10 17:18:11 +0800

    Update vennplot.py

--- overlapviz/venn/vennplot.py
+++ overlapviz/venn/vennplot.py
@@ -4,19 +4,25 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from matplotlib.colors import to_rgba
 import matplotlib.cm as cm
+import pandas as pd
 
 # Load geometric data from pickle file
-with open('geometric_data_v2.pkl', 'rb') as f:
+with open('geometric_data_v3.pkl', 'rb') as f:
     loaded_dict = pickle.load(f)
 
 # Create figure and axis with 10x10 inch size
 fig, ax = plt.subplots(figsize=(10, 10))
 
 # Extract data from loaded dictionary
-df_edges = loaded_dict['shape404']['set_edge']
-set_label = loaded_dict['shape404']['set_label']
-df_label = loaded_dict['shape404']['region_label']
+df_edges = loaded_dict['shape403']['set_edge']
+set_label = loaded_dict['shape403']['set_label']
+df_label = loaded_dict['shape403']['region_label']
 
+df = pd.read_csv("plotdata_overlaps.csv")
+df['set_names']  = df['set_names'].str.replace(' & ', '_')
+print(df)
+df_label = pd.merge(df_label, df, left_on='id', right_on='set_names', how='left')
+print(df_label)
 # Step 1: Calculate global boundaries for coordinate axis limits
 x_min, x_max = df_edges['X'].min(), df_edges['X'].max()
 y_min, y_max = df_edges['Y'].min(), df_edges['Y'].max()
@@ -25,12 +31,25 @@ y_min, y_max = df_edges['Y'].min(), df_edges['Y'].max()
 for name, group in df_edges.groupby('id'):
     verts = group[['X', 'Y']].values
     # Generate colors using viridis colormap
-    colors = [cm.viridis(i / len(df_edges.groupby('id'))) for i in range(len(df_edges.groupby('id')))]
+
+    unique_ids = df_edges['id'].unique()  #  list(df_edges.groupby('id').groups.keys())
+    id_to_index = {id_val: i for i, id_val in enumerate(unique_ids)}
+    colors = [cm.viridis(i / len(unique_ids)) for i in range(len(unique_ids))]
+
+    index = id_to_index[name]  
     poly = Polygon(verts, 
-                   closed=True, 
-                   facecolor=to_rgba(colors[int(name) % len(colors)], 0.4),  # 0.4 = transparency
-                   edgecolor='none',  # No edge for now
-                   zorder=1)          # Place in background
+                closed=True, 
+                facecolor=to_rgba(colors[index], 0.4),
+                edgecolor='none',
+                zorder=1)
+
+    # colors = [cm.viridis(i / len(df_edges.groupby('id'))) for i in range(len(df_edges.groupby('id')))]
+    # poly = Polygon(verts, 
+    #                closed=True, 
+    #                #facecolor=to_rgba(colors[int(name) % len(colors)], 0.4),  # 0.4 = transparency
+    #                edgecolor='none',  # No edge for now
+    #                zorder=1)          # Place in background
+
     ax.add_patch(poly)
 
 # Step 3: Draw borders for all areas (no fill) ---
@@ -46,7 +65,7 @@ for name, group in df_edges.groupby('id'):
 
 # Step 4: Add region labels
 for _, row in df_label.iterrows():
-    ax.text(row['X'], row['Y'], str(row['id']), 
+    ax.text(row['X'], row['Y'], str(row['size']), 
             fontsize=10, ha='center', va='center', fontweight='bold')
 
 # Step 5: Add set labels
